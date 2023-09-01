b028448657505627da8563267405ed115f2a4b0e
Andrey-Ruiz <ruizandrey90@gmail.com>
2023-09-01 11:31:32 -0500

    se agregaron procesos

--- ejercicoparcial.dart
+++ ejercicoparcial.dart
@@ -1,25 +1,83 @@
 //Andrey Alessandro Ruiz Mass 
+import 'dart:io';
 
 class Producto{
-  String nombre;
-  String tipo;
-  double precio;
-  int dia;
-  int mes;
-  int year;
+  String Nombre;
+  String Tipo;
+  double Precio;
+  int Dia;
+  int Mes;
+  int Year;
 
-  Producto(this.nombre, this.tipo, this.precio, this.dia, this.mes, this.year);
+  Producto(this.Nombre, this.Tipo, this.Precio, this.Dia, this.Mes, this.Year);
   
 
   void descuento(int Dcompra, int Dmes, int Dyear){
-    int cadu = dia - Dcompra;
-    if(cadu <=15){
-      double desc = precio*0.15;
-      precio = precio - desc;
+    int cadu = Dia - Dcompra;
+    if(cadu <=15 && Mes == Dmes && Year == Dyear){
+      double desc = Precio*0.15;
+      Precio = Precio - desc;
     }
   }
 }
 
 void main(){
-
+  String? nom = '';
+  String? tipo = '';
+  double precio = 0.0;
+  int dia = 0;
+  int mes = 0;
+  int year = 0;
+  List factu = [];
+  double i = 0;
+  print('Bienvenido, porfavor ingrese dia de pago');
+  print('Dia:');
+  String? d = stdin.readLineSync();
+  int diax = int.parse(d!);
+  print('Mes:');
+  String? m = stdin.readLineSync();
+  int mesx = int.parse(m!);
+  print('Año:');
+  String? y = stdin.readLineSync();
+  int yearx = int.parse(y!);
+  int opc = 0;
+  do{
+    print('1. Agregar producto');
+    print('2. Ver total actual');
+    print('3. Terminar y cancelar total');
+    String? aux = stdin.readLineSync();
+    opc = int.parse(aux!);
+    switch(opc){
+      case 1:
+      print('Nombre:');
+      String? n = stdin.readLineSync();
+      nom = n;
+      print('Tipo:');
+      String? t = stdin.readLineSync();
+      tipo = t;
+      print('Precio:');
+      String? p = stdin.readLineSync();
+      precio = double.parse(p!);
+      print('Dia:');
+      String? d = stdin.readLineSync();
+      dia = int.parse(d!);
+      print('Mes:');
+      String? m = stdin.readLineSync();
+      mes = int.parse(m!);
+      print('Año:');
+      String? y = stdin.readLineSync();
+      year = int.parse(y!);
+      var miproducto = Producto(nom!, tipo!, precio, dia, mes, year);
+      miproducto.descuento(diax, mesx, yearx);
+      factu.add(miproducto.Nombre);
+      i = i+miproducto.Precio;
+      break;
+      case 2:
+      print(factu);
+      print('precio total actual es: $i');
+      break;
+    }
+    
+  }while(opc != 3);
+  print('precio final es $i');
 }
